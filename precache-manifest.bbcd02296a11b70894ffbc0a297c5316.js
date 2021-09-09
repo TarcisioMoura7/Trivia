28086a0b5aee99fc772e32c7fd0b2055b5980f74
Tarcisio Moura <bigtharcisiomoura@gmail.com>
2021-09-09 12:42:59 -0300

    Updates

---
+++ precache-manifest.bbcd02296a11b70894ffbc0a297c5316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c25b40956e6922ce3d6ecfad1d826af",
+    "url": "/Trivia/index.html"
+  },
+  {
+    "revision": "769f01396afc35342923",
+    "url": "/Trivia/static/css/main.0bf56819.chunk.css"
+  },
+  {
+    "revision": "1028226566305b745a71",
+    "url": "/Trivia/static/js/2.ab9579b8.chunk.js"
+  },
+  {
+    "revision": "8dc647c3672b30664d97feee03a4be2d",
+    "url": "/Trivia/static/js/2.ab9579b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "769f01396afc35342923",
+    "url": "/Trivia/static/js/main.7401d356.chunk.js"
+  },
+  {
+    "revision": "7b9d59fd4dad6263b7ee",
+    "url": "/Trivia/static/js/runtime-main.f0d2c5ec.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia/static/media/trivia.466d153e.png"
+  }
+]);
